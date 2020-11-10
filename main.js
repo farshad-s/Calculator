e71b48c7dc6d2d393485089a44e46f6b2878cc5c
Farshad Shafii <farshad.shafii@gmail.com>
2020-11-10 11:20:24 +0000

    targeted the buttons

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 let calcBottom = document.getElementsByClassName("calc-display-bottom")[0];
 let calcTop = document.getElementsByClassName("calc-display-top")[0];
+let number = document.getElementsByClassName("number");
+let calcOp = document.getElementsByClassName("operation");
+let clear = document.getElementsByClassName("clear");
+let deleteLast = document.getElementsByClassName("delete")[0];
 
 calcBottom.innerText = "";
 calcTop.innerText = "";
