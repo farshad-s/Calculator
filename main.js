e3e39b6f563136e21169d953df098f0be3e12087
Farshad Shafii <farshad.shafii@gmail.com>
2020-11-10 11:17:38 +0000

    targeted top and bottom calc results

---
+++ main.js
@@ -0,0 +1,5 @@
+let calcBottom = document.getElementsByClassName("calc-display-bottom")[0];
+let calcTop = document.getElementsByClassName("calc-display-top")[0];
+
+calcBottom.innerText = "";
+calcTop.innerText = "";
