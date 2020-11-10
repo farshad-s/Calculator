a352abd0d10b33192d521de9e89763a0983d9331
Farshad Shafii <farshad.shafii@gmail.com>
2020-11-10 11:22:14 +0000

    created clear calculator function

--- main.js
+++ main.js
@@ -2,8 +2,17 @@ let calcBottom = document.getElementsByClassName("calc-display-bottom")[0];
 let calcTop = document.getElementsByClassName("calc-display-top")[0];
 let number = document.getElementsByClassName("number");
 let calcOp = document.getElementsByClassName("operation");
-let clear = document.getElementsByClassName("clear");
+let clear = document.getElementsByClassName("clear")[0];
 let deleteLast = document.getElementsByClassName("delete")[0];
 
-calcBottom.innerText = "";
-calcTop.innerText = "";
+// calcBottom.innerText = "";
+// calcTop.innerText = "";
+
+function clearCalc() {
+  clear.addEventListener("click", function () {
+    calcBottom.innerText = "";
+    calcTop.innerText = "";
+  });
+}
+
+clearCalc();
